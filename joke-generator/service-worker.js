fea59189dede664c3deb43dd9a15884dc11014f8
Maxweal2000 <112349745+Maxweal2000@users.noreply.github.com>
2025-02-20 16:20:17 +0000

    service worker done on joke-generator app now Working on PWA (mobile application)

--- joke-generator/service-worker.js
+++ joke-generator/service-worker.js
@@ -1,4 +1,58 @@
+//Cache Essentials Assets 
+const cacheName = "joke-generator-v1";
+const cacheAssets = [
+    "/index.html",
+    "/manifest.json",
+    "/src/icons/joke-48x48.png",
+    "/src/icons/joke-72x72.png",
+    "/src/main.jsx",
+    "/src/App.jsx",
+    "/src/style.css"
+];
+
+
+
+//Call Install event
 self.addEventListener("install", (event) => {
-console.log("Service worker installing...");
+    console.log("Service worker Installling...");
 // Put your code for caching assets, etc. here
+
+    event.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log("Caching assets...");
+            return cache.addAll(cacheAssets);
+        })
+    );
 });
+
+//Serve Cached Assets (Fetch Event)
+self.addEventListener("fetch", (event) => {
+    console.log("Fetching:", event.request.url);
+
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+//delete old cache (Activate Event)
+self.addEventListener("activate", (event) => {
+    console.log("Service Worker activating...");
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== cacheName) {
+                        console.log("Deleting old cache:", cache);
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+
+
--- joke-generator/src/main.jsx
+++ joke-generator/src/main.jsx
@@ -5,15 +5,15 @@ import App from './App.jsx'
 
 if ("serviceWorker" in navigator) {
 window.addEventListener("load", () => {
-navigator.serviceWorker
-.register("./service-worker.js")
-.then((registration) => {
-console.log("Service Worker registered! Scope: ", registration.scope);
-})
-.catch((err) => {
-console.log("Service Worker registration failed: ", err);
-});
-});
+  navigator.serviceWorker
+    .register("./service-worker.js")
+    .then((registration) => {
+      console.log("Service Worker registered! Scope: ", registration.scope);
+    })
+    .catch((err) => {
+      console.log("Service Worker registration failed: ", err);
+    });
+  });
 }
 
 createRoot(document.getElementById('root')).render(
